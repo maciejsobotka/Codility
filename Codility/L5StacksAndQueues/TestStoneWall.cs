474cf96e15db7c8fd5fe2d6d269d495390cd2df9
maciejsobotka <sobot92@tlen.pl>
2015-04-09 20:15:28 +0200

    Lesson 5 Task 4 100%.

--- Codility/L5StacksAndQueues/TestStoneWall.cs
+++ Codility/L5StacksAndQueues/TestStoneWall.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace Codility.L5StacksAndQueues
 {
@@ -7,8 +8,71 @@ namespace Codility.L5StacksAndQueues
     public class TestStoneWall
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestStoneWallMethod1()
         {
+            SolutionStoneWall s = new SolutionStoneWall();
+            int[] H = { 8, 8, 5, 7, 9, 8, 7, 4, 8 };
+            var result = s.solution(H);
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void TestStoneWallMethod2()
+        {
+            SolutionStoneWall s = new SolutionStoneWall();
+            int[] H = { 8 };
+            var result = s.solution(H);
+            Assert.AreEqual(1, result);
+        }
+    }
+
+    class SolutionStoneWall
+    {
+        /// <summary>
+        /// <para>Given a zero-indexed array H of N positive integers specifying</para>
+        /// <para>the height of the wall, returns the minimum number of blocks</para>
+        /// <para>needed to build it.</para>
+        /// </summary>
+        /// <param name="H">Array with heights of the wall.</param>
+        /// <returns>Minimum number of blocks needed to build the wall.</returns>
+        public int solution(int[] H)
+        {
+            Stack<int> heights = new Stack<int>();
+            int blocks = 0;
+
+            foreach (var i in H)
+            {
+                if (heights.Count == 0) 
+                {
+                    blocks++;
+                    heights.Push(i);
+                }
+                else
+                {
+                    if (heights.Peek() > i)
+                    {
+                        blocks++;
+                        heights.Pop();
+                        while (heights.Count != 0)
+                        {
+                            if (heights.Peek() < i) break;
+                            if (heights.Peek() == i)
+                            {
+                                blocks--;
+                                break;
+                            }
+                            heights.Pop();
+                        }
+                        heights.Push(i);
+                    }
+                    if (heights.Peek() < i)
+                    {
+                        blocks++;
+                        heights.Push(i);
+                    }
+                }
+            }
+            return blocks;
         }
     }
 }
