3c4839e4be054c788f18ea250b4d2831b883e5b0
maciejsobotka <sobot92@tlen.pl>
2015-04-09 22:46:50 +0200

    Lesson 8 painless 100%.

---
+++ Codility/L8PrimeAndCompositeNumbers/TestCountFactors.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility.L8PrimeAndCompositeNumbers
+{
+    [TestClass]
+    public class TestCountFactors
+    {
+        [TestMethod]
+        public void TestCountFactorsMethod1()
+        {
+            SolutionCountFactors s = new SolutionCountFactors();
+            var result = s.solution(24);
+            Assert.AreEqual(8, result);
+        }
+
+        [TestMethod]
+        public void TestCountFactorsMethod2()
+        {
+            SolutionCountFactors s = new SolutionCountFactors();
+            var result = s.solution(7);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void TestCountFactorsMethod3()
+        {
+            SolutionCountFactors s = new SolutionCountFactors();
+            var result = s.solution(25);
+            Assert.AreEqual(3, result);
+        }
+    }
+
+    class SolutionCountFactors
+    {
+        /// <summary>
+        /// <para>Given a integer, returns number of it's factors.</para>
+        /// </summary>
+        /// <param name="N">Integer.</param>
+        /// <returns>Number of factors.</returns>
+        public int solution(int N)
+        {
+            int factors = 0;
+            for (int i = 1; i <= Math.Sqrt(N); i++)
+                if (N % i == 0)
+                {
+                    if (i == Math.Sqrt(N)) factors++;
+                    else factors = factors + 2;
+                }
+            return factors;
+        }
+    }
+}
---
+++ Codility/L8PrimeAndCompositeNumbers/TestMinPerimeterRectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility.L8PrimeAndCompositeNumbers
+{
+    [TestClass]
+    public class TestMinPerimeterRectangle
+    {
+        [TestMethod]
+        public void TestMinPerimeterRectangleMethod1()
+        {
+            SolutionMinPerimeterRectangle s = new SolutionMinPerimeterRectangle();
+            var result = s.solution(30); ;
+            Assert.AreEqual(22, result);
+        }
+    }
+
+    class SolutionMinPerimeterRectangle
+    {
+        /// <summary>
+        /// <para>Given an integer N, returns the minimal perimeter of any rectangle</para>
+        /// <para>whose area is exactly equal to N.</para>
+        /// </summary>
+        /// <param name="N">Integer.</param>
+        /// <returns>Minimal perimeter of any rectangle whose area is exactly equal to N.</returns>
+        public int solution(int N)
+        {
+            int min = 2 * (1 + N);
+            for (int i = 2; i <= Math.Sqrt(N); i++)
+                if (N % i == 0)
+                    min = Math.Min(min, 2 * (i + N / i));
+            return min;
+        }
+    }
+}
