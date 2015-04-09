ddb79a6c5cc20b0953bbce71ec453b39dcc36869
maciejsobotka <sobot92@tlen.pl>
2015-04-09 21:07:01 +0200

    Lesson 6 100%.

---
+++ Codility/L6Leader/TestDominator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility.L6Leader
+{
+    [TestClass]
+    public class TestDominator
+    {
+        [TestMethod]
+        public void TestDominatorMethod1()
+        {
+            SolutionDominator s = new SolutionDominator();
+            int[] A = { 3, 4, 3, 2, 3, -1, 3, 3 };
+            var result = s.solution(A); ;
+            Assert.AreEqual(6, result);
+        }
+    }
+    class SolutionDominator
+    {
+        /// <summary>
+        /// <para>given a zero-indexed array A consisting of N integers,</para>
+        /// <para>returns index of any element of array A in which</para>
+        /// <para>the dominator of A occurs.</para>
+        /// <para>The function should return −1 if array A does not have a dominator.</para>
+        /// </summary>
+        /// <param name="A">Array with N integers.</param>
+        /// <returns>Dominator of array or -1 if there's none.</returns>
+        public int solution(int[] A)
+        {
+            if (A.Length == 0) return -1;
+
+            int n = 0, index = 0, val = A[0];
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (n == 0)
+                {
+                    n++;
+                    val = A[i];
+                    index = i;
+                }
+                else if (val != A[i]) n--;
+                else n++;
+            }
+            if (n == 0) return -1;
+
+            n = 0;
+            foreach (var i in A)
+                if (i == val) n++;
+            if (n <= A.Length / 2) return -1;
+            return index;
+        }
+    }
+}
---
+++ Codility/L6Leader/TestEquiLeader.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility.L6Leader
+{
+    [TestClass]
+    public class TestEquiLeader
+    {
+        [TestMethod]
+        public void TestEquiLeaderMethod1()
+        {
+            SolutionEquiLeader s = new SolutionEquiLeader();
+            int[] A = { 4, 3, 4, 4, 4, 2 };
+            var result = s.solution(A); ;
+            Assert.AreEqual(2, result);
+        }
+    }
+
+    class SolutionEquiLeader
+    {
+        /// <summary>
+        /// <para>Given a non-empty zero-indexed array A consisting of N integers,</para>
+        /// <para>returns the number of equi leaders.</para>
+        /// </summary>
+        /// <param name="A">Array with N integers.</param>
+        /// <returns>Number of equi leaders.</returns>
+        public int solution(int[] A)
+        {
+            int equiLeaders = 0, leaderNum = 0, leaderVal = -1;
+
+            foreach (var i in A)            // finding leader value
+            {
+                if (leaderNum == 0)
+                {
+                    leaderNum++;
+                    leaderVal = i;
+                }
+                else    
+                    if (leaderVal != i)   leaderNum--;
+                    else    leaderNum++;
+            }                               // no leaders
+            if (leaderNum == 0) return equiLeaders;
+
+            int leaderCount = 0;
+            foreach (var i in A)
+                if (i == leaderVal) leaderCount++;
+                                            // not enough leader values
+            if (leaderCount <= A.Length / 2)   return equiLeaders; 
+
+            leaderNum = 0;
+            for(int i = 0; i < A.Length; ++i){
+                if(A[i] == leaderVal) leaderNum++;
+                var rLeaders = leaderCount - leaderNum;
+                if (leaderNum > (i + 1) / 2 && rLeaders > (A.Length - i - 1) / 2)
+                    equiLeaders++;
+            }
+            return equiLeaders;
+        }
+    }
+}
