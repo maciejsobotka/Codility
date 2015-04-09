5cdde0e812bfde2e1963cc9d04df2999ec158e4e
maciejsobotka <sobot92@tlen.pl>
2015-04-09 22:19:00 +0200

    Lesson 7 painless 100%.

---
+++ Codility/L7MaximumSliceProblem/TestMaxProfit.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility.L7MaximumSliceProblem
+{
+    [TestClass]
+    public class TestMaxProfit
+    {
+        [TestMethod]
+        public void TestMaxProfitMethod1()
+        {
+            SolutionMaxProfit s = new SolutionMaxProfit();
+            int[] A = { 23171, 21011, 21123, 21366, 21013, 21367 };
+            var result = s.solution(A); ;
+            Assert.AreEqual(356, result);
+        }
+    }
+
+    class SolutionMaxProfit
+    {
+        /// <summary>
+        /// <para>Returns the maximum possible profit from one transaction during</para>
+        /// <para>stock period given by array A.</para>
+        /// <para>The function should return 0 if it was impossible to gain any profit.</para>
+        /// </summary>
+        /// <param name="A">Array with stock prices.</param>
+        /// <returns>Maximum possible profit or 0 if it was impossible to gain profit.</returns>
+        public int solution(int[] A)
+        {
+            if (A.Length == 0 || A.Length == 1) return 0;
+
+            int lowestPrice = A[0], highestDiff = 0, maxProfit = 0;
+
+            for (int i = 1; i < A.Length; ++i )
+            {
+                highestDiff = Math.Max(0, A[i] - lowestPrice);
+                lowestPrice = Math.Min(lowestPrice, A[i]);
+                maxProfit = Math.Max(maxProfit, highestDiff);
+            }
+            return maxProfit;
+        }
+    }
+}
---
+++ Codility/L7MaximumSliceProblem/TestMaxSliceSum.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility.L7MaximumSliceProblem
+{
+    [TestClass]
+    public class TestMaxSliceSum
+    {
+        [TestMethod]
+        public void TestMaxSliceSumMethod1()
+        {
+            SolutionMaxSliceSum s = new SolutionMaxSliceSum();
+            int[] A = { 3, 2, -6, 4, 0 };
+            var result = s.solution(A); ;
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void TestMaxSliceSumMethod2()
+        {
+            SolutionMaxSliceSum s = new SolutionMaxSliceSum();
+            int[] A = { -4, -3, -6 };
+            var result = s.solution(A); ;
+            Assert.AreEqual(-3, result);
+        }
+    }
+
+    class SolutionMaxSliceSum
+    {
+        /// <summary>
+        /// <para>Given an array A consisting of N integers,</para>
+        /// <para>returns the maximum sum of any slice of A.</para>
+        /// </summary>
+        /// <param name="A">Array of integers.</param>
+        /// <returns>Maximum sum of any slice of A.</returns>
+        public int solution(int[] A)
+        {
+            int maxEnding = A[0], maxSlice = A[0];
+
+            for (int i = 1; i < A.Length; ++i)
+            {
+                maxEnding = Math.Max(A[i], maxEnding + A[i]);
+                maxSlice = Math.Max(maxSlice, maxEnding);
+            }
+            return maxSlice;
+        }
+    }
+}
