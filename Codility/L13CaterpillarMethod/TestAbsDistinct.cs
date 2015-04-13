ee90a5261a282ec7524d76b55bcac79b80a89749
maciejsobotka <sobot92@tlen.pl>
2015-04-13 16:59:14 +0200

    Lesson 13 painless 100%.

---
+++ Codility/L13CaterpillarMethod/TestAbsDistinct.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Codility.L13CaterpillarMethod
+{
+    [TestClass]
+    public class TestAbsDistinct
+    {
+        [TestMethod]
+        public void TestAbsDistinctMethod1()
+        {
+            SolutionAbsDistinct s = new SolutionAbsDistinct();
+            int[] A = { -5, -3, -1, 0, 3, 6 };
+            var result = s.solution(A);
+            Assert.AreEqual(5, result);
+        }
+    }
+
+    class SolutionAbsDistinct
+    {
+        /// <summary>
+        /// Returns number of distinct absolute values among the elements of the array.
+        /// </summary>
+        /// <param name="A">Given values.</param>
+        /// <returns>Absolute distinct count.</returns>
+        public int solution(int[] A)
+        {
+            HashSet<int> Set = new HashSet<int>();
+            foreach (var i in A)
+                Set.Add(i > 0 ? i : -i);
+            return Set.Count;
+        }
+    }
+}
---
+++ Codility/L13CaterpillarMethod/TestCountDistinctSlices.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codility.L13CaterpillarMethod
+{
+    [TestClass]
+    public class TestCountDistinctSlices
+    {
+        [TestMethod]
+        public void TestCountDistinctSlicesMethod1()
+        {
+            SolutionCountDistinctSlices s = new SolutionCountDistinctSlices();
+            int[] A = { 3, 4, 5, 5, 2 };
+            int M = 6;
+            var result = s.solution(M, A);
+            Assert.AreEqual(9, result);
+        }
+
+        [TestMethod]
+        public void TestCountDistinctSlicesMethod2()
+        {
+            SolutionCountDistinctSlices s = new SolutionCountDistinctSlices();
+            int[] A = { 2, 2 };
+            int M = 3;
+            var result = s.solution(M, A);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void TestCountDistinctSlicesMethod3()
+        {
+            SolutionCountDistinctSlices s = new SolutionCountDistinctSlices();
+            int[] A = { 5, 4, 3, 5, 2};
+            int M = 6;
+            var result = s.solution(M, A);
+            Assert.AreEqual(13, result);
+        }
+    }
+
+    class SolutionCountDistinctSlices
+    {
+        /// <summary>
+        /// Counts the number of distinct slices in array.
+        /// </summary>
+        /// <param name="M">Highest possible number in array.</param>
+        /// <param name="A">Array of integers.</param>
+        /// <returns>Number of distinct slices.</returns>
+        public int solution(int M, int[] A)
+        {
+            bool[] used = new bool[M + 1];
+            int front = 0, back = 0;
+            int distinctSlices = 0;
+
+            while (front < A.Length)
+            {
+                while (front < A.Length && !used[A[front]])
+                {
+                    distinctSlices += (front - back + 1);
+                    if (distinctSlices > 1000000000) return 1000000000;
+                    used[A[front]] = true;
+                    front++;
+                }
+                if (front < A.Length)
+                {
+                    while (A[back] != A[front])
+                    {
+                        used[A[back]] = false;
+                        back++;
+                    }
+                    used[A[back]] = false;
+                    back++;
+                }
+            }
+            return distinctSlices;
+        }
+    }
+}
